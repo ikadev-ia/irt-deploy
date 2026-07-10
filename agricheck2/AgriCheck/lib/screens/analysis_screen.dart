b69d6c15504279440d0061e493b867686a1eaf81
Badra <ikadev.services@gmail.com>
2026-07-10 10:03:32 +0000

    Configs ...

--- agricheck2/AgriCheck/lib/screens/analysis_screen.dart
+++ agricheck2/AgriCheck/lib/screens/analysis_screen.dart
@@ -13,12 +13,13 @@ import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 Future<String?> analyserMaladieAvecGemini(XFile imageFile) async {
   // Récupération sécurisée de la clé API Gemini
-  final String? geminiApiKey = dotenv.env['GEMINI_API_KEY'];
-  if (geminiApiKey == null || geminiApiKey.trim().isEmpty) {
-    return "Erreur : Clé GEMINI_API_KEY introuvable ou non configurée dans l'environnement.";
-  }
+  // final String? geminiApiKey = dotenv.env['GEMINI_API_KEY'];
+  // if (geminiApiKey == null || geminiApiKey.trim().isEmpty) {
+  //   return "Erreur : Clé GEMINI_API_KEY introuvable ou non configurée dans l'environnement.";
+  // }
   
-  
+  //const String geminiApiKey = "AQ.Ab8RN6JrYW7LrJ-2caeHpfOcxgMf1_LKY5qydB0h9NJalmVIig";
+  const String geminiApiKey = "AQ.Ab8RN6JrYW7LrJ-2caeHpfOcxgMf1_LKY5qydB0h9NJalmVIig";
   
 
   try {
